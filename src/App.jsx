0a2752b7c7eb3a6b72bafa957ee666fc792b1d92
himanshu8183 <hardikpatel009989@gmail.com>
2026-09-20 16:18:54 +0530

    Complete Week 3 useState state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import StudentCard from "./components/StudentCard";
 
 function App() {
-  return (
-    <div>
-      <h1>Student Management System</h1>
+  const [count, setCount] = useState(0);
+ return (
+  <div>
+    <h1>Student Management System</h1>
 
-      <StudentCard
-  name="Rahul"
-  rollNo="101"
-  course="BCA"
-/>
-    </div>
-  );
+    <p>Total Students: {count}</p>
+    <button onClick={() => setCount(count + 1)}>
+  Add Student
+</button>
+
+    <StudentCard
+      name="Rahul"
+      rollNo="101"
+      course="BCA"
+    />
+  </div>
+);
 }
 
 export default App;
